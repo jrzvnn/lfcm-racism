23d462cb7e0d04bc4e77ab841402eb78e062a994
John Glen Falceso <johnglenfalceso@protonmail.com>
2023-11-23 09:56:05 +0800

    added custome dataset

---
+++ src/training/customDataset.py
@@ -0,0 +1,132 @@
+import numpy as np
+from torch.utils.data import Dataset
+import os
+import csv
+from PIL import Image
+import customTransform
+import torch
+
+
+class CustomDatasetFCM(Dataset):
+    def __init__(self, root_dir, split, Rescale, RandomCrop, Mirror):
+        """custom dataset for FCM
+
+        Args:
+            root_dir (string): root directory filepath e.g. (dir1/dir2)
+            split (string): filepath for split data e.g. (train.csv)
+            Rescale (_type_): _description_
+            RandomCrop (_type_): _description_
+            Mirror (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        self.root_dir = root_dir
+        self.split = split
+        self.Rescale = Rescale
+        self.RandomCrop = RandomCrop
+        self.Mirror = Mirror
+        self.hidden_state_dim = 150
+        self.num_elements = 0
+        self.tweet_ids = []
+        self.labels = []
+        self.tweets = None
+        self.img_texts = None
+
+        self.get_num_elements(f"{root_dir}/{split}")
+        self.set_tweet_ids(f"{root_dir}/{split}")
+        self.set_labels(f"{root_dir}/{split}")
+
+        # initially set all tweets and img text to zeros
+        self.tweets = np.zeros(
+            (self.num_elements, self.hidden_state_dim), dtype=np.float32
+        )
+        self.img_texts = np.zeros(
+            (self.num_elements, self.hidden_state_dim), dtype=np.float32
+        )
+
+    # get number of elements or rows of split
+    def get_num_elements(self, split_filepath):
+        with open(split_filepath, "r", encoding="utf-8") as file:
+            csv_reader = csv.reader(file)
+            data = [row for row in csv_reader]
+            self.num_elements = len(data)
+
+        # assumes that split (csv) has a header so we have to decrement 1
+        if self.num_elements > 0:
+            self.num_elements -= 1
+
+    # set tweet ids from split
+    def set_tweet_ids(self, split_filepath):
+        with open(split_filepath, "r", encoding="utf-8") as file:
+            csv_reader = csv.reader(file)
+            data = [row for row in csv_reader]
+            #  remove header
+            if len(data) > 0:
+                data = data[1:]
+
+            for row in data:
+                self.tweet_ids.append(row[0])
+
+    # set labels from split
+    def set_labels(self, split_filepath):
+        with open(split_filepath, "r", encoding="utf-8") as file:
+            csv_reader = csv.reader(file)
+            data = [row for row in csv_reader]
+            #  remove header
+            if len(data) > 0:
+                data = data[1:]
+
+            for row in data:
+                self.labels.append(row[-2])
+
+    def __len__(self):
+        return len(self.tweet_ids)
+
+    def __getitem__(self, index):
+        img_name = f"{self.root_dir}/images_resized/{self.tweet_ids[index]}.jpg"
+        out_img = np.zeros((3, 299, 299), dtype=np.float32)  # tweet image
+        out_text = np.zeros(self.hidden_state_dim)  # tweet text
+        out_img_text = np.zeros(self.hidden_state_dim)  # tweet image text
+
+        # for handling image only
+        try:
+            image = Image.open(img_name)
+            width, height = image.size
+
+            if self.RandomCrop >= width or self.RandomCrop >= height:
+                image = image.resize(
+                    (int(width * 1.5), int(height * 1.5)), Image.LANCZOS
+                )
+
+            if self.Rescale != 0:
+                image = customTransform.rescale(image, self.Rescale)
+
+            if self.RandomCrop != 0:
+                image = customTransform.random_crop(image, self.RandomCrop)
+
+            if self.Mirror:
+                image = customTransform.mirror(image)
+
+            image = customTransform.preprocess_image_to_np_arr(image)
+
+            out_img = image
+        except:
+            out_img = np.zeros((3, 299, 299), dtype=np.float32)
+
+        # for handling tweet text only
+        try:
+            out_text = torch.from_numpy(np.array(self.tweets[index]).copy())
+        except:
+            out_text = torch.from_numpy(np.array(self.tweets[index]).copy())
+
+        # for handling image text only
+        try:
+            out_img_text = torch.from_numpy(np.array(self.img_texts[index]).copy())
+        except:
+            out_img_text = torch.from_numpy(np.array(self.img_texts[index]).copy())
+
+        label = torch.from_numpy(np.array([int(self.labels[index])]))
+        label = label.type(torch.LongTensor)
+
+        return torch.from_numpy(out_img.copy()), out_img_text, out_text, label
--- src/training/lfcm.py
+++ src/training/lfcm.py
@@ -14,7 +14,7 @@ class LFCM(nn.Module):
         c['lstm_hidden_state_dim'] = 150
         c['gpu'] = gpu
         self.cnn = inceptionv3Cutie.customInception3(pretrained=True, aux_logits=False)
-        self.mm = FCM(c)
+        self.mm = NewFCM(c)
         self.initialize_weights()
 
     def forward(self, image, img_text, tweet, comment):
@@ -44,8 +44,45 @@ class LFCM(nn.Module):
                 m.bias.data.zero_()
 
 
+class OldModel(nn.Module):
+    def __init__(self, gpu=0):
 
-class FCM(nn.Module):
+        super().__init__()
+        c = {}
+        c['num_classes'] = 2
+        c['lstm_hidden_state_dim'] = 150
+        c['gpu'] = gpu
+        self.cnn = inceptionv3Cutie.customInception3(pretrained=True, aux_logits=False)
+        self.mm = OldFCM(c)
+        self.initialize_weights()
+
+    def forward(self, image, img_text, tweet):
+
+        i = self.cnn(image) # * 0 # CNN
+        it = img_text * 0  # Img Text Input
+        tt = tweet * 0   # Tweet Text Input
+        x = self.mm(i, it, tt) # Multimodal net
+        return x
+
+    def initialize_weights(self):
+        for m in self.mm.modules(): # Initialize only mm weights
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm1d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                m.weight.data.normal_(0, 0.01)
+                m.bias.data.zero_()
+
+
+class NewFCM(nn.Module):
 
     def __init__(self, c):
         super().__init__()
@@ -78,6 +115,45 @@ class FCM(nn.Module):
         x = torch.cat((it, tt), dim=1)
         x = torch.cat((i, x), dim=1)
         x = torch.cat((tc, x), dim=1)
+
+        # ARCH-1 4fc
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
+        x = self.fc4(x)
+
+        return x
+    
+
+class OldFCM(nn.Module):
+
+    def __init__(self, c):
+        super().__init__()
+
+        # Unimodal
+        self.cnn_fc1 = BasicFC(2048, 1024)
+        self.img_text_fc1 = BasicFC(c['lstm_hidden_state_dim'], 1024)
+        self.tweet_text_fc1 = BasicFC(c['lstm_hidden_state_dim'], 1024)
+
+        # Multimodal
+        self.fc1 = BasicFC(1024*3, 2048)
+        self.fc2 = BasicFC(2048, 1024)
+        self.fc3 = BasicFC(1024, 512)
+        self.fc4 = nn.Linear(512, c['num_classes'])
+
+    def forward(self, i, it, tt):
+        # tt = F.dropout(tt, p=0.5, training=self.training)
+        # it = F.dropout(it, p=0.5, training=self.training)
+        # tc = F.dropout(it, p=0.5, training=self.training)
+
+        # Separate process
+        i = self.cnn_fc1(i)
+        it = self.img_text_fc1(it)
+        tt = self.tweet_text_fc1(tt)
+
+        # Concatenate
+        x = torch.cat((it, tt), dim=1)
+        x = torch.cat((i, x), dim=1)
 
         # ARCH-1 4fc
         x = self.fc1(x)
